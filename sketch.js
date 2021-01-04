c4b8712018a5945398992e638a06f423cee9ae65
suhani <s.marfatia08@gmail.com>
2021-01-04 17:44:14 +0530

    add platform

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+     plant=new Box(200,200,10,10)
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -49,7 +49,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+    plant.display();
     box3.display();
     box4.display();
     pig3.display();
